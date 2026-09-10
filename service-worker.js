0ca7eb2cd399c0c3acf63ed4ca2b3ccdb478f492
kszasz <kathi_stegmann@hotmail.com>
2026-09-10 16:29:22 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='familienplanner-v19-test';
+const CACHE_NAME='familienplanner-v19-2';
 const APP_SHELL=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
